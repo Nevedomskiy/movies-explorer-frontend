fb136cd3c74ff96b99f4b02066fdaae50312751c
Vasiliy.Nevedomskiy <nevedomsky.vasily@yandex.ru>
2023-08-07 17:27:31 +0400

    исправляю ошибки при изменении профиля

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -138,7 +138,7 @@ function Profile({ logOut, editProfile, textServerError }) {
                   </button>
                   :
                   <div className='profile__block-save'>
-                     {((textServerError !== '') && (!email.isActive || !name.isActive)) && <p className='profile__server-error errors__element'>{textServerError}</p>}
+                     {((textServerError !== '') && (email.isActive || name.isActive)) && <p className='profile__server-error errors__element'>{textServerError}</p>}
                      <button
                         type="button"
                         onClick={handleSave}
